dafffe78c13a2d92b7ef5a44a31b3325468e8909
thenraja01 <thenwthen@gmail.com>
2025-08-27 17:44:46 +0530

    error also loading

--- src/Routers/BaseRoutes.jsx
+++ src/Routers/BaseRoutes.jsx
@@ -2,15 +2,17 @@ import { Outlet,Route,Routes } from "react-router-dom";
 import Hero from "../website/component/Templates/Hero";
 import Home from "../website/component/Templates/Home";
 import About from '../website/component/Templates/About'
-import Utilities from "../Auth/Signin/Utilities";
+import Error from "./Error";
 import Templates from '../website/component/Templates/Templates'
 export default function baseRoutes() {
     
     return(
-     <Routes element={<Templates/>}>
+     <Routes >
+      <Route element={<Templates/>}>
         <Route index element={<Templates/>}/>
       <Route path="home" element={<Home />}/>
         <Route path="about" element={<About />} />
+      </Route>
        <Route path="*" element={<Error/>}/>    
     </Routes>
   );
